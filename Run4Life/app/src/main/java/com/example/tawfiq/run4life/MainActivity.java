da8e5aec701393148de690802a6f81226321a2cc
Tawfiq <tawjaw@gmail.com>
2015-03-20 21:34:05 -0400

    working service! note: create service from android studio templates(Add->New->Service) next step implement location listener

--- Run4Life/app/src/main/java/com/example/tawfiq/run4life/MainActivity.java
+++ Run4Life/app/src/main/java/com/example/tawfiq/run4life/MainActivity.java
@@ -1,33 +1,39 @@
 package com.example.tawfiq.run4life;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
 
-    LocalService mService;
+public class MainActivity extends Activity {
+    Button button = null;
+    MyService mService;
     boolean mBound = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(mOnClickRandomListener);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // Bind to LocalService
-        Intent intent = new Intent(this, LocalService.class);
+        Intent intent = new Intent(this, MyService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
 
@@ -40,7 +46,6 @@ public class MainActivity extends ActionBarActivity {
             mBound = false;
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -62,6 +67,7 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     /** Called when a button is clicked (the button in the layout file attaches to
      * this method with the android:onClick attribute) */
     public void onButtonClick(View v) {
@@ -73,6 +79,21 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(this, "number: " + num, Toast.LENGTH_SHORT).show();
         }
     }
+
+    private Button.OnClickListener mOnClickRandomListener = new Button.OnClickListener() {
+
+        /**
+         * Called when the Record button is clicked (not the Pause button). Calls the RecordService
+         * to start recording.
+         */
+        @Override
+        public void onClick(View v) {
+
+            onButtonClick(v);
+            button.setText("123");
+        }
+
+    };
     /** Defines callbacks for service binding, passed to bindService() */
     private ServiceConnection mConnection = new ServiceConnection() {
 
@@ -80,7 +101,7 @@ public class MainActivity extends ActionBarActivity {
         public void onServiceConnected(ComponentName className,
                                        IBinder service) {
             // We've bound to LocalService, cast the IBinder and get LocalService instance
-            LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+            MyService.LocalBinder binder = (MyService.LocalBinder) service;
             mService = binder.getService();
             mBound = true;
         }
@@ -89,7 +110,5 @@ public class MainActivity extends ActionBarActivity {
         public void onServiceDisconnected(ComponentName arg0) {
             mBound = false;
         }
-
     };
-
 }
