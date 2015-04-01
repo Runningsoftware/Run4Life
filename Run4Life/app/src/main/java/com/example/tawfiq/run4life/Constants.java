dc8ea99f20560488f94fd649aecef6460446052a
Tawfiq <tawjaw@gmail.com>
2015-04-01 06:50:38 -0400

    working foreground

--- Run4Life/app/src/main/java/com/example/tawfiq/run4life/Constants.java
+++ Run4Life/app/src/main/java/com/example/tawfiq/run4life/Constants.java
@@ -2,10 +2,19 @@ package com.example.tawfiq.run4life;
 
 public class Constants {
     public interface ACTION {
-        public static String MAIN_ACTION = "com.example.tawfiq.run4life.MyService.action.main";
+        public static String MAIN_ACTION = "run4life.MyService.action.main";
 
-        public static String STARTFOREGROUND_ACTION = "com.example.tawfiq.run4life.MyService.action.startforeground";
-        public static String STOPFOREGROUND_ACTION = "com.example.tawfiq.run4life.MyService.action.stopforeground";
+        public static String STARTFOREGROUND_ACTION = "run4life.MyService.action.startforeground";
+        public static String STOPFOREGROUND_ACTION = "run4life.MyService.action.stopforeground";
+
+
+        public static String PREV_ACTION = "run4life.MyService.action.prev";
+        public static String PLAY_ACTION = "run4life.MyService.action.play";
+        public static String NEXT_ACTION = "run4life.MyService.action.next";
+    }
+
+    public interface NOTIFICATION_ID {
+        public static int FOREGROUND_SERVICE = 101;
     }
 
 
--- Run4Life/app/src/main/java/com/example/tawfiq/run4life/MyService.java
+++ Run4Life/app/src/main/java/com/example/tawfiq/run4life/MyService.java
@@ -1,8 +1,13 @@
 package com.example.tawfiq.run4life;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -10,7 +15,8 @@ import java.util.List;
 import java.util.Random;
 
 public class MyService extends Service {
-
+    Notification notification = null;
+    NotificationCompat.Builder builder = null;
 
     public MyService() {
         Log.i(TAG, "constructor service called");
@@ -38,12 +44,58 @@ public class MyService extends Service {
             notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                     | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
-        } else if (intent.getAction().equals(
-                Constants.ACTION.STOPFOREGROUND_ACTION)) {
-            Log.i(TAG, "Received Stop Foreground Intent");
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                    notificationIntent, 0);
+
+
+            Intent playIntent = new Intent(this, MyService.class);
+            playIntent.setAction(Constants.ACTION.PLAY_ACTION);
+            PendingIntent pplayIntent = PendingIntent.getService(this, 0,
+                    playIntent, 0);
+
+            Intent nextIntent = new Intent(this, MyService.class);
+            nextIntent.setAction(Constants.ACTION.NEXT_ACTION);
+            PendingIntent pnextIntent = PendingIntent.getService(this, 0,
+                    nextIntent, 0);
+
+            Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.icontwitter24x24);
+
+            builder = new NotificationCompat.Builder(this)
+                    .setContentTitle("Run4Life")
+                    .setTicker("Run4Life")
+                    .setContentText("Run Updates")
+                    .setSmallIcon(R.drawable.icontwitter24x24)
+                    .setLargeIcon(
+                            Bitmap.createScaledBitmap(icon, 128, 128, false))
+                    .setContentIntent(pendingIntent)
+                    .setOngoing(true)
+                    .addAction(android.R.drawable.ic_media_pause, "Pause Run",
+                            pplayIntent)
+                    .addAction(android.R.drawable.ic_delete, "Stop Run",
+                            pnextIntent);
+            notification = builder.build();
+            startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
+                    notification);}
+            else if (intent.getAction().equals(Constants.ACTION.PLAY_ACTION)) {
+                Log.i(TAG, "Clicked pause");
+            builder.setContentText("run paused");
+            notification = builder.build();
+            startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
+                    notification);
+            } else if (intent.getAction().equals(Constants.ACTION.NEXT_ACTION)) {
+                Log.i(TAG, "Clicked stop");
+            builder.setContentText("run stopped");
+            notification = builder.build();
+            startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
+                    notification);
             stopForeground(true);
-            stopSelf();
-        }
+            } else if (intent.getAction().equals(
+                    Constants.ACTION.STOPFOREGROUND_ACTION)) {
+                Log.i(TAG, "Received Stop Foreground Intent");
+                stopForeground(true);
+                stopSelf();
+            }
         return START_STICKY;
     }
 
